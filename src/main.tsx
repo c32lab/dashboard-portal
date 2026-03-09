f6bbcf3e5482572789210c386d9172715e15b812
阿玛尼 <amani@o3cloud.com>
2026-03-09 18:34:52 +0800

    feat: frontend error tracking and performance monitoring (issue #228)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import { initErrorTracker } from './utils/errorTracker'
+import { initPerformanceMonitor } from './utils/performanceMonitor'
+
+initErrorTracker()
+initPerformanceMonitor()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
